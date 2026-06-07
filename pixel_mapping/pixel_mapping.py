8d8d63c19512924c8e6fb26fc0cd59f4f0d1667b
Kaustav Biswas <140887068+N3M3515069@users.noreply.github.com>
2026-06-07 12:44:21 +0530

    Refactor measure_silicon_diameter for additional parameters
    Updated measure_silicon_diameter function to accept lumerical_nm and reference_px parameters for diameter calculation in nanometers.

--- pixel_mapping/pixel_mapping.py
+++ pixel_mapping/pixel_mapping.py
@@ -2,7 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 from utils import measure_diameter
 
-def measure_silicon_diameter(path, scale_factor):
+def measure_silicon_diameter(path, lumerical_nm= None, reference_px= None):
 
     gray = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 
@@ -10,7 +10,9 @@ def measure_silicon_diameter(path, scale_factor):
 
     diameter_px = measure_diameter(binary_matrix= binary / 255)
     print(f"Diameter (pixels): {diameter_px}")
-    print(f"Diameter (nm): {diameter_px * scale_factor:.4f}")
+
+    if lumerical_nm != None and reference_px != None:
+        print(f"Diameter (nm): {diameter_px * (lumerical_nm / reference_px):.4f}")
 
     _, ax = plt.subplots(1,2, figsize= (12,6))
     ax[0].imshow(gray, cmap= 'gray')
@@ -21,8 +23,13 @@ def measure_silicon_diameter(path, scale_factor):
     ax[1].set_title('Binary (Otsu)')
     plt.show()
 
+    return diameter_px
+
 if __name__ == '__main__':
-    measure_silicon_diameter(
-        path= r'path/to/your/cropped_circle.png', # update this path
-        scale_factor= 5/15
-        )
+    # first function call to find the diamter of the original silicon
+    reference_px= measure_silicon_diameter(path=r'C:\Vscode_Python\Pixel_Mapping\silicons\cropped_fig28_.740113.jpg') # use the original silicon img path
+
+    # second function call to apply the found diamter value and lumerical_nm value in order to get diameter in nm
+    measure_silicon_diameter(path=r'C:\Vscode_Python\Pixel_Mapping\silicons\cropped_generated_0.png', # use the generated silicon img path
+                             reference_px=reference_px,
+                             lumerical_nm=5) # physical diameter of the real silicon hole as designed in Lumerical (in nm)
